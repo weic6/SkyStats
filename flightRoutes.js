c99d0db4c1d73b8cea4862645eb920bf0b7b2f5e
weic6 <wei.rarr@gmail.com>
2024-09-26 11:31:02 -0500

    use subquery to early filter and reduce rows

--- flightRoutes.js
+++ flightRoutes.js
@@ -35,20 +35,28 @@ router.get("/delay-history/city/:city", async (req, res) => {
   const cacheKey = `delay-history:city:${city}`;
   // console.log(`cacheKey: ${cacheKey}`);
   const sql = `
-          SELECT
-            ORIGIN_AIRPORT,
-            a.CITY AS ori_city,
-            DESTINATION_AIRPORT,
-            a1.CITY AS des_city,
-            SUM(CASE WHEN ARRIVAL_DELAY > 0 THEN 1 ELSE 0 END) / COUNT(*) AS delay_rate
-          FROM Flights f
-          JOIN Airports a ON a.IATA_CODE = f.ORIGIN_AIRPORT
-          JOIN Airports a1 ON a1.IATA_CODE = f.DESTINATION_AIRPORT
-          WHERE ORIGIN_AIRPORT NOT LIKE '1__' AND DESTINATION_AIRPORT NOT LIKE '1__' AND a.city= ?
-          GROUP BY ORIGIN_AIRPORT, DESTINATION_AIRPORT
-
-          LIMIT 50
-        `;
+SELECT
+  ORIGIN_AIRPORT,
+  a.CITY AS ori_city,
+  DESTINATION_AIRPORT,
+  a1.CITY AS des_city,
+  SUM(CASE WHEN ARRIVAL_DELAY > 0 THEN 1 ELSE 0 END) / COUNT(*) AS delay_rate
+FROM
+  (SELECT
+     ORIGIN_AIRPORT,
+     DESTINATION_AIRPORT,
+     SUM(CASE WHEN ARRIVAL_DELAY > 0 THEN 1 ELSE 0 END) / COUNT(*) AS delay_rate
+   FROM Flights
+   WHERE
+     ORIGIN_AIRPORT NOT LIKE '1__' AND
+     DESTINATION_AIRPORT NOT LIKE '1__'
+   GROUP BY ORIGIN_AIRPORT, DESTINATION_AIRPORT
+  ) f
+JOIN Airports a ON a.IATA_CODE = f.ORIGIN_AIRPORT
+JOIN Airports a1 ON a1.IATA_CODE = f.DESTINATION_AIRPORT
+WHERE a.city= ?
+LIMIT 50
+`;
   checkCache(req, res, cacheKey, sql, [city]);
 });
 
